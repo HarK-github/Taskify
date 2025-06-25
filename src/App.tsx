8dfc1791c07319096bc7df34253b863cb2c2274a
Harshit Kandpal <kandpalhar@gmail.com>
2025-06-25 22:40:13 +0530

    Merged utkumap chnges

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
 import Register from './Register.tsx'
-import './App.css'
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-import "../htmlImplementation/style/profile.css";
-// import Profile from './Profile';
-// import Footer from './Footer';
-import Login from './components/Login';
 
- 
-const App: React.FC = () => {
+import './App.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "../htmlImplementation/style/profile.css";
+import Profile from './Profile';
+import Footer from './Footer';
+import Login from './components/Login.tsx';
+
+function App() {
+
   return (
     <>
-    
-    {/* <Profile/>
-    <Footer /> */}
+    <div>
+    <Profile/>
+    <Footer />
     <Login />
-    
+    </div>
       </>
       
   )
 }
 
-
-export default App;
+export default App
