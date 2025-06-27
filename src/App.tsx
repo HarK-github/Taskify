45a8d979c0b740c16e90108f2165f49e5d2e212b
chaitraliz-D <chaitralizode7@gmail.com>
2025-06-27 21:30:53 +0530

    commit6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import Register from '../htmlImplementation/Register.tsx'
-
+import MyNotes from './notes.tsx'
 function App() {
 
   return (
-    
-      <Register/>
-      <MyNotes/>
+    <>
+    <Register/>
+      <MyNotes/> 
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-<<<<<<< HEAD
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
-=======
+import App from './App.tsx'
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
->>>>>>> 85ba077ac6b322935bc9a71d0843be3ad4374ced
+import App from './App.tsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
